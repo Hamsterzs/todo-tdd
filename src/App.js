5744f26f13c4318ca160603f9874c638298e4047
abel <abel090713@gmail.com>
2021-01-01 18:11:54 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [list, setList] = useState([])
+  const [input, setInput] = useState("")
+
+  const items = list.map((item, index) => <li key={index}>{item}</li>)
+
+  const addItem = () => {
+    if (input === "") return
+    const search = list.find(item => item === input)
+    if (search) return
+    setList([...list, input])
+    setInput("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="title">Todo List</h1>
+      <input type="text" className="input" placeholder="list item here" value={input} onChange={e => setInput(e.target.value)} />
+      <button className="button" onClick={() => addItem()}>Add Item</button>
+      <ul className="list">
+        {items.length > 0 ? items : "No items added yet"}
+      </ul>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,79 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from "react"
+import { mount } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("todo list", () => {
+  let wrapper
+  let button
+  let list
+  let input
+
+  const addToList = (newItem, input, button) => {
+    input.simulate("change", { target: { value: newItem } })
+    button.simulate("click")
+  }
+
+  beforeEach(() => {
+    wrapper = mount(<App />)
+    button = wrapper.find(".button")
+    list = wrapper.find(".list")
+    input = wrapper.find(".input")
+  })
+
+  it("renders a title", () => {
+    expect(wrapper.find(".title").text()).toBe("Todo List")
+  })
+
+  it("renders a text input", () => {
+    expect(input.length).toBe(1)
+  })
+
+  it("renders an add item button", () => {
+    expect(button.length).toBe(1)
+  })
+
+  it("renders todo list", () => {
+    expect(list.length).toBe(1)
+  })
+
+  it("renders an empty list message if it is so", () => {
+    expect(list.text()).toBe("No items added yet")
+  })
+
+  it("changes input", () => {
+    input.simulate("change", { target: { value: "buy pasta" } })
+    const inputUpdated = wrapper.find(".input")
+    expect(inputUpdated.props().value).toBe("buy pasta")
+  })
+
+  it("rejects empty strings", () => {
+    button.simulate("click")
+    expect(wrapper.find("li").length).toBe(0)
+  })
+
+  it("appends an item to list", () => {
+    addToList("buy pasta", input, button)
+    expect(wrapper.find("li").at(wrapper.find("li").length - 1).text()).toBe("buy pasta")
+  })
+
+  it("resets input on submit", () => {
+    addToList("buy pasta", input, button)
+    expect(wrapper.find(".input").props().value).toBe("")
+  })
+
+  it("appends many items to list", () => {
+    addToList("buy pasta", input, button)
+    addToList("finish remodeling", input, button)
+    addToList("go to school", input, button)
+    expect(wrapper.containsAllMatchingElements(["buy pasta", "go to school", "finish remodeling"])).toEqual(true)
+  })
+
+  it("does not accept duplicates", () => {
+    addToList("buy pasta", input, button)
+    addToList("finish remodeling", input, button)
+    addToList("buy pasta", input, button)
+    const numberOfPasta = wrapper.find("li").filterWhere(item => item.text() === "buy pasta")
+    expect(numberOfPasta.length).toBe(1)
+  })
+
+})
